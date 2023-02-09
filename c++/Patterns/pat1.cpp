8ce5ed7da59f0320961467ead30d2bc8e9c90857
DavidJennicson <85760802+DavidJennicson@users.noreply.github.com>
2023-02-09 21:57:34 +0530

    checked

--- c++/Patterns/pat1.cpp
+++ c++/Patterns/pat1.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
+    int a,b;
+    cout<<"Enter number of rows";
+    cin>>a;
+    cout<<"Enter number of stars per row";
+    cin>>b;
+    for (int i=1;i<=a;i++)
+    {
+        for (int j=1;j<=b;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
